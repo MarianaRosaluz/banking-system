9955dcac62b4cb8a2404aa824ab20efe3e8e4335
MarianaRosaluz <marianarosaluz@yahoo.com>
2021-11-04 18:44:24 -0300

    mapping controllers

---
+++ api/src/main/java/br/rosaluz/banking/system/controller/AccountController.java
@@ -0,0 +1,9 @@
+package br.rosaluz.banking.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/banking/system/account", produces="application/json")
+public class AccountController {
+}
---
+++ api/src/main/java/br/rosaluz/banking/system/controller/PaymentController.java
@@ -0,0 +1,9 @@
+package br.rosaluz.banking.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/banking/system/payment", produces="application/json")
+public class PaymentController {
+}
